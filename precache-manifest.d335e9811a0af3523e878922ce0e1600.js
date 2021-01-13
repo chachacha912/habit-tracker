f98d2e0124c2596f6f61d0b519e63bb54be61f72
chachacha912 <dkwkdnwk@naver.com>
2021-01-13 16:10:46 +0900

    Updates

---
+++ precache-manifest.d335e9811a0af3523e878922ce0e1600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc14f53c60ca7f742485cdc1852e8482",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "b913994b315ce1cfc1ce",
+    "url": "/habit-tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "b5f20f3a78348fc3a1c3",
+    "url": "/habit-tracker/static/js/2.e07c0dca.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit-tracker/static/js/2.e07c0dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b913994b315ce1cfc1ce",
+    "url": "/habit-tracker/static/js/main.d6e72650.chunk.js"
+  },
+  {
+    "revision": "5d759b3fde7c32f59156",
+    "url": "/habit-tracker/static/js/runtime-main.0e3ad41c.js"
+  }
+]);
